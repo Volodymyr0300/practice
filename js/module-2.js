643708b88657e1fc63e6fbab23fc9c27c482491f
Volodymyr Poliakov <vladimir03002@gmail.com>
2023-01-05 22:38:34 +0100

    practise arr

--- js/module-2.js
+++ js/module-2.js
@@ -331,14 +331,136 @@
 // }
 // console.log(checkPassword('jqueryismyjam'));
 
-function checkStorage(available, ordered) {
+// function checkStorage(available, ordered) {
+//   // Change code below this line
+//   if (ordered === 0) {
+//     return 'Your order is empty!';
+//   }
+//   if (ordered > available) {
+//     return 'Your order is too large, not enough goods in stock!';
+//   }
+//   return 'The order is accepted, our manager will contact you';
+//   // Change code above this line
+// }
+
+// // Оголоси змінну fruits і присвой їй масив фруктів - рядків "apple", "plum", "pear" і "orange".
+// // ТЕСТИ
+// // Оголошена змінна fruits
+// // Значення змінної fruits - це масив ["apple", "plum", "pear", "orange"]
+// const fruits = ['apple', 'plum', 'pear', 'orange'];
+
+// const fruits = ['apple', 'plum', 'pear', 'orange'];
+// const firstElement = fruits[0];
+// console.log(firstElement);
+// const secondElement = fruits[2];
+// console.log(secondElement);
+// const lastElement = fruits[fruits.length - 1];
+// console.log(lastElement);
+// // Change code below this line
+
+// const fruits = ['apple', 'plum', 'pear', 'orange'];
+// fruits[1] = 'peach';
+// fruits[3] = 'banana';
+// fruits.length;
+
+// Напиши функцію getExtremeElements(array), яка приймає один параметр array - масив елементів довільної довжини.
+// Функція повинна повертати масив з двох елементів - першого і останнього елемента параметра array.
+// function getExtremeElements(array) {
+//   const arr1 = array.slice(0, 1);
+//   const arr2 = array.slice(array.length - 1);
+//   const arr = arr1.concat(arr2);
+//   return arr;
+// }
+// console.log(getExtremeElements(fruits));
+// console.log(getExtremeElements([1, 2, 3, 4, 5])); /* повертає [1, 5] */
+// console.log(
+//   getExtremeElements(['Earth', 'Mars', 'Venus']),
+// ); /*  повертає ["Earth", "Venus"] */
+// console.log(
+//   getExtremeElements(['apple', 'peach', 'pear', 'banana']),
+// ); /*  повертає ["apple", "banana"] */
+
+// Доповни код функції splitMessage(message, delimiter) таким чином, щоб вона повертала у змінній words результат поділу
+// рядка message за роздільником delimiter - масив рядків.
+// function splitMessage(message, delimiter) {
+//   let words;
+//   // Change code below this line
+//   words = message.split(delimiter);
+//   // Change code above this line
+//   return words;
+// }
+// console.log(
+//   splitMessage('Mango hurries to the train', ' '),
+// ); /* повертає ["Mango", "hurries", "to", "the", "train"] */
+// console.log(splitMessage('Mango', '')); /* повертає ["M", "a", "n", "g", "o"] */
+// console.log(
+//   splitMessage('best_for_week', '_'),
+// ); /* повертає ["best", "for", "week"] */
+
+// Сервісу гравірування прикрас потрібна функція, яка б автоматично рахувала ціну гравірування,
+// залежно від кількості слів і ціни за слово.
+// Оголошена функція calculateEngravingPrice(message, pricePerWord).
+// Ця функція приймає рядок, що складається зі слів, розділених лише пробілами (параметр message)
+// та ціну гравірування одного слова (параметр pricePerWord).
+// Напиши тіло функції, щоб вона повертала загальну вартість гравірування усіх слів в рядку.
+// function calculateEngravingPrice(message, pricePerWord) {
+//   // Change code below this line
+//   let sum = message.split(' ').length * pricePerWord;
+//   return sum;
+//   // Change code above this line
+// }
+// console.log(
+//   calculateEngravingPrice('JavaScript is in my blood', 10),
+// ); /* повертає 50 */
+// console.log(
+//   calculateEngravingPrice('JavaScript is in my blood', 20),
+// ); /* повертає 100 */
+// console.log(
+//   calculateEngravingPrice('Web-development is creative work', 40),
+// ); /* повертає 160 */
+// console.log(
+//   calculateEngravingPrice('Web-development is creative work', 20),
+// ); /* повертає 80 */
+
+// // Доповни код функції makeStringFromArray(array, delimiter) таким чином,
+// // щоб вона повертала у змінній string результат з'єднання елементів масиву array з роздільником delimiter - рядок.
+// function makeStringFromArray(array, delimiter) {
+//   let string;
+//   // Change code below this line
+//   string = array.join(delimiter);
+//   // Change code above this line
+//   return string;
+// }
+// console.log(
+//   makeStringFromArray(['Mango', 'hurries', 'to', 'the', 'train'], ' '),
+// ); /* повертає "Mango hurries to the train" */
+// console.log(
+//   makeStringFromArray(['M', 'a', 'n', 'g', 'o'], ''),
+// ); /* повертає "Mango" */
+// console.log(
+//   makeStringFromArray(['top', 'picks', 'for', 'you'], '_'),
+// ); /* повертає "top_picks_for_you" */
+
+// Термін slug - це людино-зрозумілий унікальний ідентифікатор,
+// який використовується у веб-розробці для створення читабельних URL-адрес.
+// Наприклад, замість того, щоб користувач побачив в адресному рядку mysite.com/posts/1q8fh74tx,
+// можна зробити slug з назви статті. В результаті адреса буде приємнішою для сприйняття: mysite.com/posts/arrays-for-begginers.
+// Увага
+// Slug - це завжди рядок у нижньому регістрі, слова якого розділені тире.
+// Напиши функцію slugify(title), яка приймає заголовок статті, параметр title, і повертає slug, створений з цього рядка.
+// Значенням параметра title будуть рядки, слова яких розділені лише пробілами
+// Усі символи slug повинні бути у нижньому регістрі
+// Всі слова slug повинні бути розділені тире
+function slugify(title) {
   // Change code below this line
-  if (ordered === 0) {
-    return 'Your order is empty!';
-  } else if (ordered > available) {
-    return 'Your order is too large, not enough goods in stock!';
-  } else {
-    return 'The order is accepted, our manager will contact you';
-  }
   // Change code above this line
 }
+console.log(
+  slugify('Arrays for begginers'),
+); /* повертає "arrays-for-begginers" */
+console.log(
+  slugify('English for developer'),
+); /* повертає "english-for-developer" */
+console.log(
+  slugify('Ten secrets of JavaScript'),
+); /* повертає "ten-secrets-of-javascript" */
