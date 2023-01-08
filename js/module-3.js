a2a4c25d437e4a594936ca1524e367185bfb3f30
Volodymyr Poliakov <vladimir03002@gmail.com>
2023-01-08 21:54:31 +0100

    practice in objects, do some tasks

--- js/module-3.js
+++ js/module-3.js
@@ -1,8 +1,416 @@
-const arr = [1, 2, 3];
-console.log(arr);
-arr.hello = 'world';
-console.log(arr);
-console.table(arr);
-for (const i of arr) {
-  console.log(i);
-}
+// !
+// const arr = [1, 2, 3];
+// console.log(arr);
+// arr.hello = 'world';
+// console.log(arr);
+// console.table(arr);
+// for (const i of arr) {
+//   console.log(i);
+// }
+
+// !
+// const book = {
+//   title: 'The Last Kingdom',
+//   author: 'Bernard Cornwell',
+//   genres: ['historical prose', 'adventure'],
+//   isPublic: true,
+//   rating: 8.38,
+// };
+// console.log(book);
+// console.table(book);
+
+// !
+// const user = {
+//   name: 'Jacques Gluke',
+//   tag: 'jgluke',
+//   location: {
+//     country: 'Jamaica',
+//     city: 'Ocho Rios',
+//   },
+//   stats: {
+//     followers: 5603,
+//     views: 4827,
+//     likes: 1308,
+//   },
+// };
+// console.log(user);
+// console.table(user);
+
+// !
+// const book = {
+//   title: 'The Last Kingdom',
+//   author: 'Bernard Cornwell',
+//   genres: ['historical prose', 'adventure'],
+//   isPublic: true,
+//   rating: 8.38,
+// };
+
+// const bookTitle = book.title;
+// console.log(bookTitle); // 'The Last Kingdom'
+
+// const bookGenres = book.genres;
+// console.log(bookGenres); // ['historical prose', 'adventurs']
+
+// const bookPrice = book.price;
+// console.log(bookPrice); // undefined
+
+// !
+// const user = {
+//   name: 'Jacques Gluke',
+//   tag: 'jgluke',
+//   location: {
+//     country: 'Jamaica',
+//     city: 'Ocho Rios',
+//   },
+//   hobbies: ['swiming', 'music', 'sci-fi'],
+// };
+
+// const location = user.location;
+// console.log(location); // Об'єкт location
+
+// const country = user.location.country;
+// console.log(country); // 'Jamaica'
+
+// console.log(user.name);
+// console.log(user.location.city);
+
+// const hobbies = user.hobbies;
+// console.log(hobbies); // ['swiming', 'music', 'sci-fi']
+
+// const firstHobby = user.hobbies[0];
+// console.log(firstHobby); // 'swiming'
+
+// const numberOfHobbies = user.hobbies.length;
+// console.log(numberOfHobbies); // 3
+
+// !
+// const book = {
+//   title: 'The Last Kingdom',
+//   author: 'Bernard Cornwell',
+//   genres: ['historical prose', 'adventure'],
+//   isPublic: true,
+//   rating: 8.38,
+// };
+
+// const bookTitle = book['title'];
+// console.log(bookTitle); // 'The Last Kingdom'
+
+// const bookGenres = book['genres'];
+// console.log(bookGenres); // ['historical prose', 'adventurs']
+
+// const propKey = 'author';
+// const bookAuthor = book[propKey];
+// console.log(bookAuthor); // 'Bernard Cornwell'
+
+// !
+// const book = {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     genres: ["historical prose", "adventure"],
+//     isPublic: true,
+//     rating: 8.38,
+//   };
+
+//   book.rating = 9;
+//   book.isPublic = false;
+//   book.genres.push("драма");
+
+//   console.log(book.rating); // 9
+//   console.log(book.isPublic); // false
+//   console.log(book.genres); // ['historical prose', 'adventures', 'драма']
+
+// !
+// const book = {
+//   title: 'The Last Kingdom',
+//   author: 'Bernard Cornwell',
+//   genres: ['historical prose', 'adventure'],
+//   isPublic: true,
+//   rating: 8.38,
+// };
+
+// book.pageCount = 836;
+// book.originalLanguage = 'en';
+// book.translations = ['ua', 'ru'];
+
+// console.log(book.pageCount); // 836
+// console.log(book.originalLanguage); // 'en'
+// console.log(book.translations); // ['ua', 'ru']
+
+// console.log(book);
+// console.table(book);
+
+// !
+// const name = 'Генрі Сибола';
+// const age = 25;
+
+// const user = {
+//   name,
+//   age,
+// };
+
+// console.log(user.name); // "Генрі Сибола"
+// console.log(user.age); // 25
+
+// console.log(typeof user.name);
+// console.log(user.name);
+
+// !
+// const propName = 'name';
+// console.log(propName);
+// const user = {
+//   age: 25,
+//   // Ім'я цієї властивості буде взяте зі значення змінної propName
+//   [propName]: 'Генрі Сибола',
+// };
+// console.log(user);
+
+// console.log(user.name); // 'Генрі Сибола'
+
+// !
+// // ✅ Логічно і синтаксично згруповані сутності
+// const bookShelf = {
+//   books: ['The Last Kingdom', 'Dream Guardian'],
+//   // Це метод об'єкта
+//   getBooks() {
+//     console.log('Цей метод буде повертати всі книги - властивість books');
+//   },
+//   // Це метод об'єкта
+//   addBook(bookName) {
+//     console.log('Цей метод буде додавати нову книгу у властивість books');
+//   },
+// };
+
+// // Виклики методів
+// bookShelf.getBooks();
+// bookShelf.addBook('Нова книга');
+
+// !
+// const bookShelf = {
+//   books: ['The Last Kingdom'],
+//   getBooks() {
+//     console.log(this);
+//   },
+// };
+
+// // Перед крапкою знаходиться об'єкт bookShelf,
+// // тому, викликаючи метод, this буде зберігати посилання на нього.
+// bookShelf.getBooks(); // {books: ['The Last Kingdom'], getBooks: f}
+
+// !
+// const object1 = {
+//   name: 'John',
+//   age: 38,
+//   friends: {
+//     name1: 'Bill',
+//     name2: 'Smit',
+//     getNameOfFriend() {
+//       console.log(this);
+//     },
+//   },
+// };
+// console.log(object1);
+
+// !
+// const bookShelf = {
+//   books: ['The Last Kingdom'],
+//   getBooks() {
+//     return this.books;
+//   },
+//   addBook(bookName) {
+//     this.books.push(bookName);
+//   },
+//   removeBook(bookName) {
+//     const bookIndex = this.books.indexOf(bookName);
+//     this.books.splice(bookIndex, 1);
+//   },
+// };
+
+// console.log(bookShelf.getBooks()); // ["The Last Kingdom"]
+// bookShelf.addBook('The Mist');
+// bookShelf.addBook('Dream Guardian');
+// console.log(bookShelf.getBooks()); // ['The Last Kingdom', 'The Mist', 'Dream Guardian']
+// bookShelf.removeBook('The Mist');
+// console.log(bookShelf.getBooks()); // ['The Last Kingdom', 'Dream Guardian']
+
+// !!!
+// ???
+// !!!
+// const book = {
+//   title: 'The Last Kingdom',
+//   author: 'Bernard Cornwell',
+//   genres: ['historical prose', 'adventure'],
+//   rating: 8.38,
+// };
+
+// const keys = Object.keys(book);
+// console.log(keys);
+
+// const values = Object.values(book);
+// console.log(values);
+
+// for (const key of keys) {
+//   console.log(key);
+//   console.log(book[key]);
+// }
+
+// for (const value of values) {
+//   console.log(value);
+// }
+
+// !!!
+// ???
+// !!!
+// const book = {
+//   title: 'The Last Kingdom',
+//   author: 'Bernard Cornwell',
+//   genres: ['historical prose', 'adventure'],
+//   rating: 8.38,
+// };
+// const keys = Object.keys(book);
+
+// for (const key of keys) {
+//   // Ключ
+//   console.log(key);
+//   // Значення властивості
+//   console.log(book[key]);
+// }
+
+// !
+// const animal = {
+//   legs: 4,
+// };
+
+// const dog = Object.create(animal);
+
+// dog.name = 'Манго';
+
+// console.log(dog); // {name: 'Манго'}
+// console.log(dog.name); // 'Манго'
+// console.log(dog.legs); // 4
+
+// !
+// const book = {
+//   title: 'The Last Kingdom',
+//   author: 'Bernard Cornwell',
+//   rating: 8.38,
+// };
+// const keys = Object.keys(book);
+// console.log(keys); // ['title', 'author', 'rating']
+
+// const values = Object.values(book);
+// console.log(values); // ['The Last Kingdom', 'Bernard Cornwell', 8.38]
+
+// !
+// const goods = {
+//   apples: 6,
+//   grapes: 3,
+//   bread: 4,
+//   cheese: 7,
+// };
+
+// const values = Object.values(goods); // [6, 3, 4, 7]
+
+// let total = 0;
+
+// for (const value of values) {
+//   total += value;
+// }
+
+// console.log(total); // 20
+
+// !!!
+// ???
+// !!!
+// const book = {
+//   title: 'The Last Kingdom',
+//   author: 'Bernard Cornwell',
+//   rating: 8.38,
+// };
+// const keys = Object.keys(book);
+// console.log(keys); // ['title', 'author', 'rating']
+
+// const values = Object.values(book);
+// console.log(values); // ['The Last Kingdom', 'Bernard Cornwell', 8.38]
+
+// const entries = Object.entries(book); // !!!
+// console.log(entries);
+// // [["title", "The Last Kingdom"], ["author", "Bernard Cornwell"], ["rating", 8.38]]
+
+// !
+// const books = [
+//   {
+//     title: 'The Last Kingdom',
+//     author: 'Bernard Cornwell',
+//     rating: 8.38,
+//   },
+//   {
+//     title: 'На березі спокійних вод',
+//     author: 'Роберт Шеклі',
+//     rating: 8.51,
+//   },
+//   {
+//     title: 'Сон смішної людини',
+//     author: 'Федір Достоєвський',
+//     rating: 7.75,
+//   },
+// ];
+
+// for (const book of books) {
+//   // Об'єкт книги
+//   console.log(book);
+//   // Назва
+//   console.log(book.title);
+//   // Автор
+//   console.log(book.author);
+//   // Рейтинг
+//   console.log(book.rating);
+// }
+
+// const bookNames = [];
+
+// for (const book of books) {
+//   bookNames.push(book.title);
+// }
+
+// console.log(bookNames); // ["The Last Kingdom", "На березі спокійних вод", "Сон смішної людини"]
+
+// let totalRating = 0;
+
+// for (const book of books) {
+//   totalRating += book.rating;
+// }
+
+// const averageRating = (totalRating / books.length).toFixed(1);
+// console.log(averageRating); // 8.2
+
+// !
+// Присвой змінній apartment об'єкт, який описує квартиру з наступними характеристиками:
+// imgUrl - фотографія, значення "https://via.placeholder.com/640x480";
+// descr - опис, значення "Spacious apartment in the city center";
+// rating - рейтинг, значення 4;
+// price - ціна, значення 2153;
+// tags - метаінформація, масив ["premium", "promoted", "top"].
+const apartment = {
+  imgUrl: 'https://via.placeholder.com/640x480',
+  descr: 'Spacious apartment in the city center',
+  rating: 4,
+  price: 2153,
+  tags: ['premium', 'promoted', 'top'],
+};
+console.log(apartment);
+console.table(apartment);
+// Оголошена змінна apartment
+// Значення змінної apartment - це об'єкт
+// Об'єкт містить властивість imgUrl
+// Значення властивості imgUrl - це рядок "https://via.placeholder.com/640x480"
+// Об'єкт містить властивість descr
+// Значення властивості descr - це рядок "Spacious apartment in the city center"
+// Об'єкт містить властивість rating
+// Значення властивості rating - це число 4
+// Об'єкт містить властивість price
+// Значення властивості price - це число 2153
+// Об'єкт містить властивість tags
+// Значення властивості tags - це масив ["premium", "promoted", "top"]
+
+// !
+// !
