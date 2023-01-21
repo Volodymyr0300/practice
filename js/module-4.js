f0d2dea697187e20b0a4fc559b6ca5ba7d87e7c0
Volodymyr Poliakov <vladimir03002@gmail.com>
2023-01-21 13:24:56 +0100

    some practice with arrowFunc and callbackFunc

--- js/module-4.js
+++ js/module-4.js
@@ -1,1 +1,300 @@
-console.log('hello');
+// !
+// // Колбек-функція
+// function greet(name) {
+//   console.log(`Ласкаво просимо ${name}.`);
+// }
+
+// // Функція вищого порядку
+// function registerGuest(name, callback) {
+//   console.log(`Реєструємо гостя ${name}.`);
+//   callback(name);
+// }
+
+// registerGuest('Манго', greet);
+
+// !
+// function registerGuest(name, callback) {
+//   console.log(`Реєструємо гостя ${name}.`);
+//   callback(name);
+// }
+
+// // Передаємо інлайн функцію greet у якості колбека
+// registerGuest('Манго', function greet(name) {
+//   console.log(`Ласкаво просимо ${name}.`);
+// });
+
+// // Передаємо інлайн функцію notify у якості колбека
+// registerGuest('Полі', function notify(name) {
+//   console.log(`Шановний(а) ${name}, ваш номер буде готовий за 30 хвилин.`);
+// });
+
+// !
+// // function processCall(recipient) {
+// //   // Імітуємо доступність абонента випадковим числом
+// //   const isRecipientAvailable = Math.random() > 0.5;
+
+// //   if (!isRecipientAvailable) {
+// //     console.log(`Абонент ${recipient} недоступний, залиште повідомлення.`);
+// //     // Логіка активації автовідповідача
+// //   } else {
+// //     console.log(`З'єднуємо з ${recipient}, очікуйте...`);
+// //     // Логіка прийняття дзвінка
+// //   }
+// // }
+
+// // processCall('Манго');
+
+// function processCall(recipient, onAvailable, onNotAvailable) {
+//   // Імітуємо доступність абонента випадковим числом
+//   const isRecipientAvailable = Math.random() > 0.5;
+//   console.log(isRecipientAvailable);
+
+//   if (!isRecipientAvailable) {
+//     onNotAvailable(recipient);
+//     return;
+//   }
+
+//   onAvailable(recipient);
+// }
+
+// function takeCall(name) {
+//   console.log(`З'єднуємо з ${name}, очікуйте...`);
+//   // Логіка прийняття дзвінка
+// }
+
+// function activateAnsweringMachine(name) {
+//   console.log(`Абонент ${name} недоступний, залиште повідомлення.`);
+//   // Логіка активації автовідповідача
+// }
+
+// function leaveHoloMessage(name) {
+//   console.log(`Абонент ${name} недоступний, записуємо голограму.`);
+//   // Логіка запису голограми
+// }
+
+// processCall('Манго', takeCall, activateAnsweringMachine);
+// processCall('Полі', takeCall, leaveHoloMessage);
+
+// !
+// function printValue(value) {
+//   console.log(value);
+// }
+
+// function prettyPrint(value) {
+//   console.log('Logging value: ', value);
+// }
+
+// function repeat(n, action) {
+//   for (let i = 0; i < n; i += 1) {
+//     action(i);
+//   }
+// }
+
+// // Передаємо printValue як callback-функцію
+// repeat(3, printValue);
+// // 0
+// // 1
+// // 2
+
+// // Передаємо prettyPrint як callback-функцію
+// repeat(3, prettyPrint);
+// // Logging value: 0
+// // Logging value: 1
+// // Logging value: 2
+
+// !
+// const numbers = [5, 10, 15, 20, 25];
+
+// // Класичний for
+// // for (let i = 0; i < numbers.length; i += 1) {
+// //   console.log(`Індекс ${i}, значення ${numbers[i]}`);
+// // }
+
+// // Метод перебирання forEach
+// numbers.forEach(function (number, index) {
+//   console.log(`Індекс ${index}, значення ${number}`);
+// });
+
+// !
+// const testArrowFunc = name => {
+//   console.log(`Hello ${name}`);
+// };
+
+// testArrowFunc('Dave');
+
+// !
+// const add = (a, b, c) => {
+//   console.log(a, b, c);
+//   return a + b + c;
+// };
+
+// !
+// const add = (a, b, c) => a + b + c;
+
+// !
+// const numbers = [5, 10, 15, 20, 25];
+
+// // Оголошення функції
+// numbers.forEach(function (number, index) {
+//   console.log(`Індекс ${index}, значення ${number}`);
+// });
+
+// // Анонімна стрілочна функція
+// numbers.forEach((number, index) => {
+//   console.log(`Індекс ${index}, значення ${number}`);
+// });
+
+// !
+// const numbers = [5, 10, 15, 20, 25];
+
+// const logMessage = (number, index) => {
+//   console.log(`Індекс ${index}, значення ${number}`);
+// };
+
+// numbers.forEach(logMessage);
+
+// !
+// // Декларативний підхід
+// const numbers = [1, 2, 3, 4, 5];
+// const filteredNumbers = numbers.filter(value => value > 3);
+// console.log(filteredNumbers); // [4, 5]
+
+// !
+// // Доповни код таким чином, щоб у змінній result був результат виконання функції makePizza,
+// // а у змінній pointer було посилання на функцію makePizza.
+// function makePizza() {
+//   return 'Your pizza is being prepared, please wait.';
+// }
+// // Change code below this line
+
+// const result = makePizza();
+// const pointer = makePizza;
+
+// // Оголошена функція makePizza
+// // Оголошена змінна result
+// // Значення змінної result - це рядок "Your pizza is being prepared, please wait."
+// // Значення змінної result отримане за допомогою виклику функції
+// // Оголошена змінна pointer
+// // Значення змінної pointer - це посилання на функцію makePizza
+
+// !
+// // Доповни функцію makeMessage таким чином,
+// // щоб вона очікувала другим параметром (параметр callback) колбек-функцію і повертала її виклик.
+// // Функція deliverPizza або makePizza буде передаватися як колбек і очікувати аргументом ім'я готової піци, що доставляється.
+// function deliverPizza(pizzaName) {
+//   return `Delivering ${pizzaName} pizza.`;
+// }
+
+// function makePizza(pizzaName) {
+//   return `Pizza ${pizzaName} is being prepared, please wait...`;
+// }
+
+// // Chande code below this line
+// function makeMessage(pizzaName, callback) {
+//   return callback(pizzaName);
+// }
+
+// // Оголошена функція deliverPizza
+// // Оголошена функція makePizza
+// // Оголошена функція makeMessage
+// // Функція makeMessage приймає два параметри, названі відповідно до завдання, pizzaName і callback
+// console.log(makeMessage('Royal Grand', makePizza)); // повертає рядок "Pizza Royal Grand is being prepared, please wait..."
+// console.log(makeMessage('Ultracheese', deliverPizza)); // повертає рядок "Delivering Ultracheese pizza."
+
+// !
+// Доповни другий виклик функції makePizza(pizzaName, callback),
+// передавши другим аргументом інлайн колбек-функцію eatPizza(pizzaName),
+// яка логує рядок "Eating pizza <назва піци>".
+function makePizza(pizzaName, callback) {
+  console.log(`Pizza ${pizzaName} is being prepared, please wait...`);
+  callback(pizzaName);
+}
+
+makePizza('Royal Grand', function deliverPizza(pizzaName) {
+  console.log(`Delivering pizza ${pizzaName}.`);
+});
+// Change code below this line
+
+makePizza('Ultracheese', eatPizza(pizzaName));
+
+// Оголошена функція makePizza
+// Функція makePizza приймає два параметри
+// Другим аргументом під час виклику makePizza("Ultracheese") передана функція eatPizza з єдиним параметром pizzaName
+
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
