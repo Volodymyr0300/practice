6123bdb02e10d5c53dfde606727751f3a5920ad4
Volodymyr Poliakov <vladimir03002@gmail.com>
2023-02-01 21:38:14 +0100

    do some examles

---
+++ js/module-6.js
@@ -0,0 +1,98 @@
+// !
+// const listWithId = document.querySelector('#menu');
+// listWithId.style.textTransform = 'uppercase';
+// listWithId.style.fontSize = '24px';
+// console.log(listWithId);
+
+// const listWithClass = document.querySelector('.menu');
+// console.log(listWithClass);
+
+// const menuItemsByTagName = document.querySelectorAll('li');
+// console.log(menuItemsByTagName);
+
+// const menuItemsByClass = document.querySelectorAll('.menu-item');
+// console.log(menuItemsByClass);
+
+// const firstMenuItem = document.querySelector('.menu-item');
+// firstMenuItem.style.color = 'tomato';
+// console.log(firstMenuItem);
+
+// !
+// //===================================================
+// const message = document.querySelector('#message');
+// console.log(message.value); // Default textarea message
+
+// //===================================================
+// const activeLink = document.querySelector('.btn.active');
+// console.log(activeLink.href); // https://s.codepen.io/about
+
+// //===================================================
+// const image = document.querySelector('.image');
+// console.log(image.src); // https://placeimg.com/640/480/animals
+// image.src = 'https://placeimg.com/640/480/tech';
+
+// !
+// const text = document.querySelector('.article-text');
+// console.log(text.textContent); // text inside p.article-text
+
+// const title = document.querySelector('.article-title');
+// title.textContent = 'Welcome to Bahamas!';
+
+// !
+// const text = document.querySelector('#paragraph');
+
+// console.log(text.classList); // ["text", "red", "big", value: "text red big"]
+
+// console.log(text.classList.contains('red')); // true
+
+// text.classList.remove('big');
+// console.log(text.classList); // ["text", "red", value: "text red"]
+
+// text.classList.add('new-class');
+// console.log(text.classList); // ["text", "red", "new-class", value: "text red new-class"]
+
+// // Can add multiple classes
+// text.classList.add('a', 'b', 'c');
+// console.log(text.classList);
+
+// text.classList.toggle('is-hidden'); // will add is-hidden class
+// text.classList.toggle('is-hidden'); // will remove is-hidden class
+
+// // classList has a forEach method
+// text.classList.forEach(cls => {
+//   console.log(cls); // text, red, new-class
+// });
+
+// !
+// const button = document.querySelector('.btn');
+
+// button.style.backgroundColor = 'teal';
+// button.style.fontSize = '24px';
+// button.style.textAlign = 'center';
+
+// console.log(button.style); // inline styles object
+
+// !
+// const image = document.querySelector('.image');
+
+// console.log(image.attributes); // NamedNodeMap {0: class, 1: src, 2: alt, length: 3}
+
+// console.log(image.hasAttribute('src')); // true
+
+// console.log(image.getAttribute('alt')); // "Lake and clouds"
+
+// image.setAttribute('alt', 'Amazing nature');
+
+// console.log(image.getAttribute('alt')); // Amazing nature
+
+// !
+const saveBtn = document.querySelector('.editor button[data-action="save"]');
+const closeBtn = document.querySelector('.editor button[data-action="close"]');
+
+console.log(saveBtn.dataset.action); //save
+console.log(closeBtn.dataset.action); //close
+
+const dishes = document.querySelectorAll('.dishes > li');
+dishes.forEach(dish => {
+  console.log(dish.dataset.id);
+});
