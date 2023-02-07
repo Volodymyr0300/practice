a3ebfa6cce5b5dedb091fed19a6772fa5f7003ba
Volodymyr Poliakov <vladimir03002@gmail.com>
2023-02-07 22:43:18 +0100

    practice of DOM and events

--- js/module-6.js
+++ js/module-6.js
@@ -127,10 +127,313 @@
 // console.log(newArr);
 
 // !
+// const list = document.querySelector('.usernames');
+
+// // Adds an item to the end of the list
+// const lastItem = document.createElement('li');
+// lastItem.textContent = 'Poly';
+// list.append(lastItem);
+
+// // Adds an item to the beginning of the list
+// const firstItem = document.createElement('li');
+// firstItem.textContent = 'Ajax';
+// list.prepend(firstItem);
+
+// // Adds a title before the list
+// const title = document.createElement('h2');
+// title.textContent = 'USERNAMES';
+// list.before(title);
+
+// // Adds a paragraph after the list
+// const text = document.createElement('p');
+// text.textContent =
+//   'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum tenetur assumenda fugiat maxime, soluta aspernatur quasi nihil in asperiores ad distinctio illo et debitis error iure voluptate numquam maiores nisi. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum tenetur assumenda fugiat maxime, soluta aspernatur quasi nihil in asperiores ad distinctio illo et debitis error iure voluptate numquam maiores nisi!';
+// list.after(text);
+
 // !
+// const text = document.querySelector('.text');
+// text.remove();
+
 // !
+// const article = document.querySelector('.article');
+// console.log(article.innerHTML);
+
+// const title = document.querySelector('.article .title');
+// console.log(title.innerHTML);
+
+// const text = document.querySelector('.article .text');
+// console.log(text.innerHTML);
+
+// const link = document.querySelector('.article .link');
+// console.log(link.innerHTML);
+
 // !
+// const title = document.querySelector('.article .title');
+// title.innerHTML = 'New and <span class="accent">improved</span> title';
+
 // !
+// const technologies = ['HTML', 'CSS', 'JavaScript', 'React', 'Node'];
+// console.log(technologies);
+// const list = document.querySelector('.list');
+
+// const markup = technologies
+//   .map(technology => `<li class="list-item">${technology}</li>`)
+//   .join('');
+// console.log(markup);
+// console.log(typeof markup);
+
+// // Check the console, you'll see a single string with HTML tags
+// console.log(markup);
+
+// // Adding all the markup in one operation
+// list.innerHTML = markup;
+
+// !
+// const article = document.querySelector('.article');
+// const htmlString = `<p class="article-text">Nullam quis ante. Vestibulum dapibus nunc ac augue. In consectetuer turpis ut velit.</p>
+//    <a class="link" href="#">Read more...</a>`;
+
+// // Replace += with = operator. See the difference?
+// // Article title is lost because we overwrite element content.
+// article.innerHTML += htmlString;
+
+// !
+// const list = document.querySelector('.list');
+
+// const newTechnologies = ['React', 'TypeScript', 'Node.js'];
+// const markup = newTechnologies
+//   .map(technology => `<li class="list-item new">${technology}</li>`)
+//   .join('');
+
+// list.insertAdjacentHTML('beforeend', markup);
+// list.insertAdjacentHTML('beforebegin', '<h2>Popular technologies</h2>');
+
+// !
+// const button = document.querySelector('.my-button');
+
+// button.addEventListener('click', () => {
+//   console.log('Button was clicked');
+// });
+
+// !
+// const button = document.querySelector('.my-button');
+
+// const handleClick = () => {
+//   console.log('Button was clicked');
+// };
+
+// button.addEventListener('click', handleClick);
+
+// !
+// const singleBtn = document.querySelector('#single');
+
+// const handleClick = () => {
+//   console.log('click event listener callback');
+// };
+
+// singleBtn.addEventListener('click', handleClick);
+
+// // ===============================================
+// const multiBtn = document.querySelector('#multiple');
+
+// const firstCallback = () => {
+//   console.log('First callback!');
+// };
+// const secondCallback = () => {
+//   console.log('Second callback!');
+// };
+// const thirdCallback = () => {
+//   console.log('Third callback!');
+// };
+
+// multiBtn.addEventListener('click', firstCallback);
+// multiBtn.addEventListener('click', secondCallback);
+// multiBtn.addEventListener('click', thirdCallback);
+
+// !
+// const addListenerBtn = document.querySelector('[data-action="add"]');
+// const removeListenerBtn = document.querySelector('[data-action="remove"]');
+// const btn = document.querySelector('#btn');
+
+// const handleClick = () => {
+//   console.log('click event listener callback');
+// };
+
+// addListenerBtn.addEventListener('click', () => {
+//   btn.addEventListener('click', handleClick);
+//   console.log('click event listener was added to btn');
+// });
+
+// removeListenerBtn.addEventListener('click', () => {
+//   btn.removeEventListener('click', handleClick);
+//   console.log('click event listener was removed from btn');
+// });
+
+// !
+// const mango = {
+//   username: 'Mango',
+//   showUsername() {
+//     console.log(this);
+//     console.log(`My username is: ${this.username}`);
+//   },
+// };
+
+// const btn = document.querySelector('.js-btn');
+
+// // ✅ Працює
+// mango.showUsername();
+// console.log(mango.showUsername());
+
+// // ❌ this буде посилатися на button, якщо використовувати showUsername як callback
+// btn.addEventListener('click', mango.showUsername); // не працює
+
+// // ✅ Не забувайте прив'язувати контекст методів об'єкта
+// btn.addEventListener('click', mango.showUsername.bind(mango));
+
+// !
+// const handleClick = event => {
+//   console.log(event);
+// };
+
+// button.addEventListener('click', handleClick);
+
+// !
+// const button = document.querySelector('.btn');
+
+// const handleClick = event => {
+//   console.log('event: ', event);
+//   console.log('event type: ', event.type);
+//   console.log('currentTarget: ', event.currentTarget);
+// };
+
+// button.addEventListener('click', handleClick);
+
+// !
+// const form = document.querySelector('.register-form');
+
+// form.addEventListener('submit', event => {
+//   event.preventDefault();
+//   const {
+//     elements: { username, password },
+//   } = event.currentTarget;
+//   console.log(username.value, password.value);
+// });
+
+// !
+// document.addEventListener('keydown', event => {
+//   console.log('Keydown: ', event);
+// });
+
+// document.addEventListener('keyup', event => {
+//   console.log('Keyup: ', event);
+// });
+
+// !
+// const clearLogBtn = document.querySelector('[data-action="clear"]');
+// const logList = document.querySelector('.log-list');
+// let keypressCounter = 1;
+
+// document.addEventListener('keydown', logMessage);
+// document.addEventListener('keyup', logMessage);
+// clearLogBtn.addEventListener('click', reset);
+
+// function logMessage({ type, key, code }) {
+//   const markup = `<div class="log-item">
+//     <span class="chip">${keypressCounter}</span>
+//     <ul>
+//       <li><b>Event</b>: ${type}</li>
+//       <li><b>Key</b>: ${key}</li>
+//       <li><b>Code</b>: ${code}</li>
+//     </ul>
+//   </div>`;
+
+//   logList.insertAdjacentHTML('afterbegin', markup);
+
+//   if (type === 'keyup') {
+//     incrementKeypressCounter();
+//   }
+// }
+
+// function reset() {
+//   keypressCounter = 1;
+//   logList.innerHTML = '';
+// }
+
+// function incrementKeypressCounter() {
+//   keypressCounter += 1;
+// }
+
+// !
+// const form = document.querySelector('.form');
+
+// form.addEventListener('submit', handleSubmit);
+
+// function handleSubmit(event) {
+//   event.preventDefault();
+//   const {
+//     elements: { login, password },
+//   } = event.currentTarget;
+
+//   if (login.value === '' || password.value === '') {
+//     return console.log('Please fill in all the fields!');
+//   }
+
+//   console.log(`Login: ${login.value}, Password: ${password.value}`);
+//   event.currentTarget.reset();
+// }
+
+// !
+// const select = document.querySelector('.pizza-select');
+// const textOutput = document.querySelector('.text-output');
+// const valueOutput = document.querySelector('.value-output');
+
+// setOutput();
+
+// select.addEventListener('change', setOutput);
+
+// function setOutput() {
+//   const selectedOptionValue = select.value;
+//   //   console.log(selectedOptionValue);
+
+//   const selectedOptionIndex = select.selectedIndex;
+//   //   console.log(selectedOptionIndex);
+
+//   const selectedOptionText = select.options[selectedOptionIndex].text;
+//   //   console.log(selectedOptionText);
+
+//   textOutput.textContent = selectedOptionText;
+//   valueOutput.textContent = selectedOptionValue;
+// }
+
+// !
+// const textInput = document.querySelector('.text-input');
+// const output = document.querySelector('.output');
+
+// textInput.addEventListener('input', event => {
+//   output.textContent = event.currentTarget.value;
+// });
+
+// !
+// const textInput = document.querySelector('.text-input');
+// const setFocusBtn = document.querySelector('[data-action="set"]');
+// const removeFocusBtn = document.querySelector('[data-action="remove"]');
+
+// setFocusBtn.addEventListener('click', () => {
+//   textInput.focus();
+// });
+
+// removeFocusBtn.addEventListener('click', () => {
+//   textInput.blur();
+// });
+
+// textInput.addEventListener('focus', () => {
+//   textInput.value = 'This input has focus';
+// });
+
+// textInput.addEventListener('blur', () => {
+//   textInput.value = '';
+// });
+
 // !
 // !
 // !
