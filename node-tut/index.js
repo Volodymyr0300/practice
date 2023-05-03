e66b2effb2146ba033c6cef3914cbaa81b2af26a
Volodymyr Poliakov <vladimir03002@gmail.com>
2023-05-03 15:08:31 -0500

    some practice with setTimeout, setInterval and Date

--- node-tut/index.js
+++ node-tut/index.js
@@ -1,20 +1,20 @@
-const Joi = require('joi');
-const shortid = require('shortid');
-const express = require('express');
+// const Joi = require('joi');
+// const shortid = require('shortid');
+// const express = require('express');
 
-// const passwordSchema = Joi.string().min(3).max(10).alphanum();
+// // const passwordSchema = Joi.string().min(3).max(10).alphanum();
 
-// console.log(passwordSchema.validate('qwer'));
+// // console.log(passwordSchema.validate('qwer'));
 
-// console.log(shortid.generate());
+// // console.log(shortid.generate());
 
-const app = express();
+// const app = express();
 
-app.use(`*`, (req, res) => {
-  console.log(`It was the request from browser`);
-  res.send(`<h1>This is HTML answer for you</h1>`);
-});
+// app.use(`*`, (req, res) => {
+//   console.log(`It was the request from browser`);
+//   res.send(`<h1>This is HTML answer for you</h1>`);
+// });
 
-const listener = app.listen(4444, () => {
-  console.log(`Web-server ran in the port ${listener.address().port}`);
-});
+// const listener = app.listen(4444, () => {
+//   console.log(`Web-server ran in the port ${listener.address().port}`);
+// });
